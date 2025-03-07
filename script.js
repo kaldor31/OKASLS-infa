2f30e578fe16d3600f118fca46828ffa9d983138
_d4n <kaldorei.simonsie@gmail.com>
2025-03-07 13:09:34 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿
+document.addEventListener('DOMContentLoaded', function() {
+    alert("Welcome to Okasls Clothing Store! We hope you love our latest collections.");
+    
+    const shopNowButton = document.getElementById('shopNowBtn');
+    shopNowButton.addEventListener('click', function() {
+        alert('Redirecting to the shop...');
+
+        window.location.href = '#'; 
+    });
+});
